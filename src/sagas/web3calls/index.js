297c0e2e100b9baba03991367a5ea2956aea75c7
Denis <denismazzu@live.it>
2018-04-11 18:07:33 +0200

    aggiunto index al package web3calls

---
+++ src/sagas/web3calls/index.js
@@ -0,0 +1,14 @@
+/*
+ * file: index.js
+ * version: 0.1
+ * type: javascript index
+ * authors: Denis Mazzucato
+ * license: MIT License
+ * warnings: package index
+ * changes: 
+ * * Denis Mazzucato | 2018/04/11 | file creation
+ */
+
+import deployCall from './web3calls';
+
+export default deployCall;
