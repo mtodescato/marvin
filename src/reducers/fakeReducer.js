522be4d728569cce75ee9a8ba39786e66e1a3c97
Denis <denismazzu@live.it>
2018-04-13 15:32:10 +0200

    added creators to the fake DuckModel implementation

--- src/reducers/fakeReducer.js
+++ src/reducers/fakeReducer.js
@@ -4,18 +4,28 @@ import DuckModel from './duckModule';
 
 export default DuckModel({
   store: 'fake-reducer',
-  types: ['ACTION_TYPE'],
-  initialState: { fake: 0 },
+  types: ['ACTION_TYPE1', 'ACTION_TYPE2'],
+  initialState: { fake: 1 },
 }).extend({
   reducer: (state, action, { types }) => {
     switch (action.type) {
-      case types.ACTION_TYPE:
+      case types.ACTION_TYPE1:
         return {
           ...state,
           fake: state.fake + 1,
+        };
+      case types.ACTION_TYPE2:
+        return {
+          ...state,
+          fake: state.fake + 2,
         };
       default:
         return state;
     }
   },
+  creators: ({ types }) => ({
+    actionType1: () => ({ type: types.ACTION_TYPE1 }),
+    actionType2: () => ({ type: types.ACTION_TYPE2 }),
+  }),
+  // selectors: ...
 });
