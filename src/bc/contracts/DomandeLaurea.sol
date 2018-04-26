7c8c6633b07e957f27cba3663dacf5250d6ea4d5
sballari <ballarinsimone@gmail.com>
2018-04-26 11:08:57 +0200

    corretti errori compilazione

--- src/bc/contracts/DomandeLaurea.sol
+++ src/bc/contracts/DomandeLaurea.sol
@@ -2,13 +2,13 @@ pragma solidity 0.4.23;
 
 contract DomandeLaurea {
   mapping(address => DomandaLaurea) private StudentContractToDomanda;
-  mapping(int => address) private IntToSudent; // solo domande in attesa
-  mapping(address => int) private StudentToInt; // solo domande in attesa
+  mapping(uint => address) private IntToSudent; // solo domande in attesa
+  mapping(address => uint) private StudentToInt; // solo domande in attesas
   uint private last = 1; // ultimo usato
 
   struct DomandaLaurea {
     bytes titoloTesi;
-    uint8 statoDomanda; // in attesa = 0 accettato = 1  o rifiutata = -1
+    int8 statoDomanda; // in attesa = 0 accettato = 1  o rifiutata = -1
     bytes dataSottomissione;
     address relatoreContract;
   }
@@ -19,7 +19,7 @@ contract DomandeLaurea {
   }
 
   function inserisciDomanda(address studentContract, bytes titoloTesi, bytes dataSottomissione, address relatoreContract)
-    onlyNewDomanda(studentContract);
+    onlyNewDomanda(studentContract)
     public
     {
       StudentContractToDomanda[studentContract] = DomandaLaurea(titoloTesi, 0, dataSottomissione, relatoreContract);
@@ -38,15 +38,15 @@ contract DomandeLaurea {
     last -= 1;
   }
 
-  function getDomanda(uint domanda) public returns( bytes, uint8, bytes, address) {
+  function getDomanda(uint domanda) public returns( bytes, int8, bytes, address) {
     require (domanda <= last);
     address stdCont = IntToSudent[domanda];
     DomandaLaurea d = StudentContractToDomanda[stdCont];
     return ( d.titoloTesi, d.statoDomanda, d.dataSottomissione, d.relatoreContract );
   }
 
-  function getDomanda(address stdC) public returns( bytes, uint8, bytes, address) {
-    require( StudentContractToDomanda != 0x0);
+  function getDomanda(address stdC) public returns( bytes, int8, bytes, address) {
+    require( StudentContractToDomanda[stdC].relatoreContract != 0x0);
     DomandaLaurea d = StudentContractToDomanda[stdC];
     return ( d.titoloTesi, d.statoDomanda, d.dataSottomissione, d.relatoreContract );
   }
