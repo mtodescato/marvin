6628d9ca86e12e2f3875d746635d403fb39d3281
Matteo Todescato <matteotod@gmail.com>
2018-04-26 16:10:16 +0200

    fix solhint

--- src/bc/contracts/DomandeLaurea.sol
+++ src/bc/contracts/DomandeLaurea.sol
@@ -1,56 +1,55 @@
 pragma solidity 0.4.23;
 
+
 contract DomandeLaurea {
-  mapping(address => DomandaLaurea) private StudentContractToDomanda;
-  mapping(uint => address) private IntToSudent; // solo domande in attesa
-  mapping(address => uint) private StudentToInt; // solo domande in attesas
-  uint private last = 1; // ultimo usato
+    mapping(address => DomandaLaurea) private studentContractToDomanda;
+    mapping(uint => address) private intToSudent; // solo domande in attesa
+    mapping(address => uint) private studentToInt; // solo domande in attesas
+    uint private last = 1; // ultimo usato
 
-  struct DomandaLaurea {
-    bytes titoloTesi;
-    int8 statoDomanda; // in attesa = 0 accettato = 1  o rifiutata = -1
-    bytes dataSottomissione;
-    address relatoreContract;
-  }
-
-  modifier onlyNewDomanda(address studentContract) {
-    require( StudentToInt[studentContract] == 0 );
-    _;
-  }
-
-  function inserisciDomanda(address studentContract, bytes titoloTesi, bytes dataSottomissione, address relatoreContract)
-    onlyNewDomanda(studentContract)
-    public
-    {
-      StudentContractToDomanda[studentContract] = DomandaLaurea(titoloTesi, 0, dataSottomissione, relatoreContract);
-      last += 1;
-      StudentToInt[studentContract] = last;
-      IntToSudent[last] = studentContract;
+    struct DomandaLaurea {
+        bytes titoloTesi;
+        int8 statoDomanda; // in attesa = 0 accettato = 1  o rifiutata = -1
+        bytes dataSottomissione;
+        address relatoreContract;
     }
 
-  function gestisciDomanda(int8 newState, uint domanda) public {
-    require( newState == -1 || newState == 1 );
-    require( domanda <= last);
-    address stdCont = IntToSudent[domanda];
-    StudentContractToDomanda[stdCont].statoDomanda = newState;
-    StudentToInt[stdCont] = 0;
-    IntToSudent[domanda] = IntToSudent[last];
-    last -= 1;
-  }
+    modifier onlyNewDomanda(address studentContract) {
+        require(StudentToInt[studentContract] == 0);
+        _;
+    }
 
-  function getDomanda(uint domanda) public returns( bytes, int8, bytes, address) {
-    require (domanda <= last);
-    address stdCont = IntToSudent[domanda];
-    DomandaLaurea d = StudentContractToDomanda[stdCont];
-    return ( d.titoloTesi, d.statoDomanda, d.dataSottomissione, d.relatoreContract );
-  }
+    function inserisciDomanda(address studentContract, bytes titoloTesi,
+    bytes dataSottomissione, address relatoreContract)
+    public
+    onlyNewDomanda(studentContract)
+    {
+        StudentContractToDomanda[studentContract] = DomandaLaurea(titoloTesi, 0, dataSottomissione, relatoreContract);
+        last += 1;
+        StudentToInt[studentContract] = last;
+        IntToSudent[last] = studentContract;
+    }
 
-  function getDomanda(address stdC) public returns( bytes, int8, bytes, address) {
-    require( StudentContractToDomanda[stdC].relatoreContract != 0x0);
-    DomandaLaurea d = StudentContractToDomanda[stdC];
-    return ( d.titoloTesi, d.statoDomanda, d.dataSottomissione, d.relatoreContract );
-  }
+    function gestisciDomanda(int8 newState, uint domanda) public {
+        require(newState == -1 || newState == 1);
+        require(domanda <= last);
+        address stdCont = IntToSudent[domanda];
+        StudentContractToDomanda[stdCont].statoDomanda = newState;
+        StudentToInt[stdCont] = 0;
+        IntToSudent[domanda] = IntToSudent[last];
+        last -= 1;
+    }
 
+    function getDomanda(uint domanda) public returns( bytes, int8, bytes, address) {
+        require(domanda <= last);
+        address stdCont = IntToSudent[domanda];
+        DomandaLaurea d = StudentContractToDomanda[stdCont];
+        return ( d.titoloTesi, d.statoDomanda, d.dataSottomissione, d.relatoreContract );
+    }
 
-
+    function getDomanda(address stdC) public returns( bytes, int8, bytes, address) {
+        require(StudentContractToDomanda[stdC].relatoreContract != 0x0);
+        DomandaLaurea d = StudentContractToDomanda[stdC];
+        return ( d.titoloTesi, d.statoDomanda, d.dataSottomissione, d.relatoreContract );
+    } 
 }
--- src/bc/contracts/FactoryMethod.sol
+++ src/bc/contracts/FactoryMethod.sol
@@ -1,5 +1,4 @@
 pragma solidity 0.4.23;
-
 import "./User.sol";
 import "./Professor.sol";
 import "./Student.sol";
@@ -7,26 +6,26 @@ import "./Admin.sol";
 import "./Ownable.sol";
 
 
-
 contract FactoryMethod {
+
     modifier validUserType(uint8 _type) {
-      require (_type<=2);
-      _;
+        require(_type <= 2);
+        _;
     }
 
     function createUser (bytes _name, bytes _surname, bytes _socialNumber, uint _serial, address owner, uint8 _type)
-      public
-      validUserType(_type)
-      returns(address)
-      {
+    public
+    validUserType(_type)
+    returns(address)
+    {
         User newUser;
-        if (_type ==0) {
+        if (_type == 0) {
             newUser = new Professor(_name, _surname, _socialNumber, _serial);
         }
-        if (_type ==1) {
+        if (_type == 1) {
             newUser = new Student(_name, _surname, _socialNumber, _serial);
         }
-        if (_type ==2) {
+        if (_type == 2) {
             newUser = new Admin(_name, _surname, _socialNumber, _serial);
         }
         newUser.transfertOwnernship(owner);
