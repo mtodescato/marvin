be7327ab9efb48faf780fc8f953217a7fff9cd0b
Matteo Todescato <matteotod@gmail.com>
2018-04-26 14:31:36 +0200

    fixed eslint on truffle test and added test globals

--- src/bc/test/prova.js
+++ src/bc/test/prova.js
@@ -2,26 +2,17 @@ const ListUsers = artifacts.require('./ListUsers.sol');
 const AdminFacade = artifacts.require('./AdminFacade.sol');
 const User = artifacts.require('./User.sol');
 
+contract('Prova List e factory', () => {
+  let adminFacadeInstance;
+  let ListUsersInstance;
 
-function cleanZeros(str) {
-  let newStr = str;
-  while (newStr.search('\u0000') !== -1) {
-    newStr = newStr.replace('\u0000', '');
-  }
-  return newStr;
-}
+  AdminFacade.deployed().then((inst) => { adminFacadeInstance = inst; });
+  ListUsers.deployed().then((inst) => { ListUsersInstance = inst; });
 
-contract ('Prova List e factory' , () => {
-  AdminFacade.deployed().then( inst => { adminFacadeInstance = inst });
-  ListUsers.deployed().then( inst => { ListUsersInstance = inst });
-
-  it ('creazione List e factory', () => {
+  it('creazione List e factory', () => {
     adminFacadeInstance.addUser('simone1', 'ballarin', 'bllsmn7580297584', 1233, 0x0656, 0);
-    ListUsersInstance.getUser.call(0).then( usr => {
-        return User.at(usr);})
-      .then( usr => usr.getSerial())
-      .then(v => assert.equal(v.toNumber() ,1233,'test inserimento professore'));
-    });
+    ListUsersInstance.getUser.call(0).then(usr => User.at(usr))
+      .then(usr => usr.getSerial())
+      .then(v => assert.equal(v.toNumber(), 1233, 'test inserimento professore'));
+  });
 });
-
-
