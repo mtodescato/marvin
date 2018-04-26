4dbfea0d002ca873a4d9f355d4ea53d73bd411c7
sballari <ballarinsimone@gmail.com>
2018-04-26 10:32:06 +0200

    creato metodo per inserire domanda in std facade

--- src/bc/contracts/StudentFacade.sol
+++ src/bc/contracts/StudentFacade.sol
@@ -1,5 +1,23 @@
 pragma solidity 0.4.23;
+import "DomandaLaurea.sol";
+import "ListUsers.sol";
+
 
 contract StudentFacade {
-  
+  DomandaLaurea private DomandeLaurea;
+  ListUsers private ListaUtenti;
+
+
+  function StudentFacade(address domandaLaurea, address listaU) public {
+    DomandeLaurea = DomandaLaurea(domandaLaurea);
+    ListaUtenti = ListUsers(listaU);
+  }
+
+
+
+  function creaDomandaLaurea(bytes titoloTesi, bytes dataSottomissione, address relatoreContract) public returns() {
+    require(ListaUtenti.getType(relatoreContract) == 1);
+    address stdC = ListaUtenti.getUser(msg.sender);
+    DomandaLaurea.inserisciDomanda(stdC, dataSottomissione, relatoreContract);
+  }
 }
