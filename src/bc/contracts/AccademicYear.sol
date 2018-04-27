d5d7049727271a9c6f1cb234d2e6b6e06ae7f32f
Matteo Todescato <matteotod@gmail.com>
2018-04-27 10:53:54 +0200

    added constructor paremeter on AccademicYear.sol

--- src/bc/contracts/AccademicYear.sol
+++ src/bc/contracts/AccademicYear.sol
@@ -6,8 +6,11 @@ contract AccademicYear {
     mapping(uint => address) private intToCdL;
     mapping(address => uint) private cdLToInt;
     uint private last = 0;
+    uint private year;
     
-    function AccademicYear() public {}
+    function AccademicYear(uint _year) public {
+        year = _year;
+    }
 
     function addCdL(address cdL) public {
         cdLToInt[cdL] = last;
