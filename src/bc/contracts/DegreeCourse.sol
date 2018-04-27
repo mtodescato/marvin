8621839c9deb34ba835673e0c5e0b7176e569062
Matteo Todescato <matteotod@gmail.com>
2018-04-27 11:15:16 +0200

    added DegreeCourse.sol

---
+++ src/bc/contracts/DegreeCourse.sol
@@ -0,0 +1,29 @@
+pragma solidity 0.4.23;
+
+
+contract DegreeCourse {
+    mapping(uint => address) private intToTeaching;
+    mapping(address => uint) private teachingToInt;
+    uint private last = 0;
+    bytes private degreeCourseName;
+    bytes private degreeCoursePresident;
+    uint8 private degreeCourseType;
+
+    function DegreeCourse(bytes _degreeCourseName, bytes _degreeCoursePresident, uint8 _degreeCourseType) public {
+        degreeCourseName = _degreeCourseName;
+        degreeCoursePresident = _degreeCoursePresident;
+        degreeCourseType = _degreeCourseType;
+    }
+
+    function addTeaching(address teaching) public {
+        teachingToInt[teaching] = last;
+        intToTeaching[last] = teaching;
+        last += 1;
+    }
+
+    function getTeaching(uint index) public view returns(address) {
+        require(index < last);
+        return intToTeaching[index];
+    }
+}
+
