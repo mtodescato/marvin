111aa2c4cb187d989dd83975de0f47fbfd248707
sballari <ballarinsimone@gmail.com>
2018-04-28 17:56:27 +0200

    inserita in studente info sul cdl con get e set

--- src/bc/contracts/Student.sol
+++ src/bc/contracts/Student.sol
@@ -3,6 +3,9 @@ import "./User.sol";
 
 
 contract Student is User {
+
+    address private degreeCourse;
+
     function Student(bytes _name, bytes _surname, bytes _socialNumber, uint _serial)
     public
     User(_name, _surname, _socialNumber, _serial)
@@ -13,4 +16,12 @@ contract Student is User {
     function insertPassedExam (address teaching, address exam) public {
         teachingToExam[teaching] = exam;
     }
+
+    function getDegreeCourse() public view returns(address) {
+        return degreeCourse;
+    }
+
+    function setDegreeCourse(address degreeC) public {
+        degreeCourse = degreeC;
+    } 
 }
