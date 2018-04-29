a893f6875dbf81fbf2c56507151b0ca97745ea17
sballari <ballarinsimone@gmail.com>
2018-04-29 16:11:34 +0200

    inserite get

--- src/bc/contracts/Teaching.sol
+++ src/bc/contracts/Teaching.sol
@@ -2,17 +2,21 @@ pragma solidity 0.4.23;
 
 
 contract Teaching {
-    
+
     mapping(address => uint) private examToInt;
     mapping(uint => address) private intToExam;
     uint private last = 0;
-    bytes private professor;
-    
-    function Teaching(bytes _professor) public {
+    address private professor;
+
+    function Teaching(address _professor) public {
         professor = _professor;
     }
 
-    function addExam(address exam) public {
+    function addExam(address exam, address sender)
+    public
+    onlyReferenceProf(sender)
+    onlyCorrectExam(exam)
+    {
         examToInt[exam] = last;
         intToExam[last] = exam;
         last += 1;
@@ -22,5 +26,26 @@ contract Teaching {
         require(index < last);
         return intToExam[index];
     }
+
+    function getReferenceProfessor() public view returns(address) {
+        return professor;
+    }
+
+    modifier onlyReferenceProf (address prof) {
+        require(professor == prof);
+        _;
+    }
+
+    function getTheachingProfessor() public view return(address) {
+      return professor;
+    }
+
+    modifier onlyCorrectExam (address exam) {
+      Exam ex = Exam(exam);
+      address exTeaching = ex.getTeaching();
+      require(address(this) == exTeaching);
+      _;
+    }
+
 
 }
