00eb1d88533652e17ea7f2f4f4cda999f4fec6af
sballari <ballarinsimone@gmail.com>
2018-04-29 18:37:27 +0200

    cambio nome a prova.js in listUsersTest

--- src/bc/test/listUsersTest.js
+++ src/bc/test/listUsersTest.js
@@ -2,17 +2,17 @@ const ListUsers = artifacts.require('./ListUsers.sol');
 const AdminFacade = artifacts.require('./AdminFacade.sol');
 const User = artifacts.require('./User.sol');
 
-contract('Prova List e factory', () => {
+contract('Testing ListUsers', () => {
   let adminFacadeInstance;
   let ListUsersInstance;
 
   AdminFacade.deployed().then((inst) => { adminFacadeInstance = inst; });
   ListUsers.deployed().then((inst) => { ListUsersInstance = inst; });
 
-  it('creazione List e factory', () => {
+  it('Test insert and get user', () => {
     adminFacadeInstance.addUser('simone1', 'ballarin', 'bllsmn7580297584', 1233, 0x0656, 0);
     ListUsersInstance.getUser().then(usr => User.at(usr))
       .then(usr => usr.getSerial())
-      .then(v => assert.equal(v.toNumber(), 1233, 'test inserimento professore'));
+      .then(v => assert.equal(v.toNumber(), 1233, 'test inserting professor'));
   });
 });
