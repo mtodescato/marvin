86f9ff3f96559ae5c3bf2a9ed92ab2d80c7cd95e
matteotodescato <matteotod@gmail.com>
2018-05-01 13:50:08 +0200

    documented StudentFacade.sol

--- src/bc/contracts/StudentFacade.sol
+++ src/bc/contracts/StudentFacade.sol
@@ -6,24 +6,39 @@ import "./Student.sol";
 import "./DegreeCourse.sol";
 
 
+/** @title Student facade */
 contract StudentFacade {
     DegreeRequests private degreeRequests;
     ListUsers private userList;
 
+    /**@dev check if a student have passed all the Teachings of his degree course.
+    *  @param student Address of the contract of the student that will get checked for all exams. 
+    */
     modifier onlyReadyStudent(address student) {
         require(checkExams(student));
         _;
     }
 
+    /**@dev Constructor of SudentFacade.
+    *  @param degreeRequestAddress Address of degreeRequest contract containig the degree requests.
+    *  @param userListAddress Address ot LisrUser contract containig users.
+    */
     function StudentFacade(address degreeRequestsAddess, address userListAddress) public {
         degreeRequests = DegreeRequests(degreeRequestsAddess);
         userList = ListUsers(userListAddress);
     }
 
+    /**@dev retuns the contract owned by the user calling the fuction.*/
     function getUserContract() public view returns(address) {
         return userList.getUser(msg.sender);
     }
 
+    /**@dev create a new degree request for a student
+    *  @param student Address of the student requesting to start the degree process.
+    *  @param thesisTitle It is the title of the thesis.
+    *  @param submissionDate Date when request is sent.
+    *  @param professor Address of the professor validating the student thesis.
+    */
     function createDegreeRequest(address student, bytes thesisTitle, bytes submissionDate, address professor)
     public
     onlyReadyStudent(student)
@@ -32,11 +47,20 @@ contract StudentFacade {
         degreeRequests.addRequest(student, thesisTitle, submissionDate, professor);
     }
 
+    /**@dev Subscribe a student to an exam.
+    *  @param student Address of the student contract.
+    *  @param exam Address of the exam.
+    */
     function subscribeToExam(address student, address exam) public {
         Exam ex = Exam(exam);
         ex.subscribe(student);
     }
 
+    /**@dev Manage a vote for un exam with the option to accept o reject it.
+    *  @param student Address of the student contract.
+    *  @param exam Address of the exam contract.
+    *  @param mark accept or reject vote, 1 for accapted and 0 for reject.
+    */
     function manageVote(address student, address exam, bool mark) public {
         Exam ex = Exam(exam);
         ex.manageVote(student, mark);
@@ -47,6 +71,9 @@ contract StudentFacade {
         }
     }
 
+    /**@dev Check if a student has passed exams far all his teachings.
+    *  @param Student Address of the student contract.
+    */
     function checkExams(address student) private view returns(bool) {
 
         Student std = Student(student);
