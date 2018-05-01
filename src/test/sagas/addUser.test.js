5dde27c1fe34182f5bcaf81b547afc58e9f11478
Francesca Lonedo <lonedofrancesca@gmail.com>
2018-05-01 14:48:10 +0200

    removed unneeded const

--- src/test/sagas/addUser.test.js
+++ src/test/sagas/addUser.test.js
@@ -1,10 +1,9 @@
-import { createStore }  from 'redux';
-import { AddUser } from 'redux';
+import { createStore } from 'redux';
+import { AddUser } from '../../reducers';
 
 describe('AddUser saga test suite', () => {
   const store = createStore(AddUser.reducer);
   it('should set store to true when success action is dispatched', () => {
-    const userAdded = false;
     store.dispatch(AddUser.creators.addUserSuccess());
     expect(store.getState()).to.have.property('userAdded', true);
   });
