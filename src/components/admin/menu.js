f84f6a02e34aca201d8849f3f63c3af77d997b34
JZambo9 <JZambo93@gmail.com>
2018-05-02 10:50:56 +0200

    entries added

--- src/components/admin/menu.js
+++ src/components/admin/menu.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { Menu as MenuGrommet, Sidebar, Header, Box } from 'grommet';
+import { Section, Heading, Accordion } from 'grommet';
 import MenuAdmin from '../../containers/admin/menuAdmin';
-import { Section, Heading, Accordion, AccordionPanel } from 'grommet';
 
 const Menu = () => (
   <Section fixed pad="medium" >
     <Heading tag="h3">
       Admin Menù
     </Heading>
-    <MenuGrommet>
-        <MenuAdmin />
-    </MenuGrommet>
+    <Accordion>
+      <MenuAdmin />
+    </Accordion>
   </Section>
 );
 
--- src/components/admin/menuEntries.js
+++ src/components/admin/menuEntries.js
@@ -3,29 +3,24 @@ import PropTypes from 'prop-types';
 import { AccordionPanel, Anchor} from 'grommet';
 import SubMenu from "./subEntries";
 
-export const entries = [
-  'Home',
-  'Create Users',
-  'List Users',
-  'Create Study Courses',
-  'List Study Courses',
-  'Create Academic Activities',
-  'List Academic Activities',
-  'Create Academic Year',
-  'List Academic Years',
-  'Degree Procedures'
-];
+export const entries = [   
+  {entry:"Users", subItems: ["List Users","Add User"]}, 
+  {entry:"Courses", subItems: ["List Study Courses","Add Study Course"]}, 
+  {entry:"Academic Activities", subItems: ["Add Academic Activity","List  Academic Activities"]}, 
+  {itentryem:"Academic Years", subItems: ["Add Academic Year","List Academic Years"]}, 
+  {entry:"Degree Procedures", subItems: ["Doe","blue"]} 
+] 
 
-const MenuEntries = ({ active = 0, action }) => (
-  entries.map((value, index) => (
-    <Anchor
-      className={index === active ? 'active' : ''}
-      onClick={() => action(index)}
-    >
-      {value.toString()}
-    </Anchor>
-  ))
-);
+const MenuEntries = ({ active = 0, action }) => ( 
+  entries.map((value, index) => ( 
+    <AccordionPanel className={index === active ? 'active' : ''} heading = {value.item.toString()}> 
+      <SubMenu /> 
+      <Anchor className='sgaaf' onClick={() => action(0)}> 
+            Non va 
+      </Anchor> 
+    </AccordionPanel> 
+  )) 
+); 
 
 MenuEntries.propTypes = {
   active: PropTypes.number,
