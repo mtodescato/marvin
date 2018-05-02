06063677f8c8d5b4130656e254fce764b06df730
JZambo9 <JZambo93@gmail.com>
2018-05-03 00:15:26 +0200

    Aggiunte props

--- src/components/admin/dashboard/courseConfirmation.js
+++ src/components/admin/dashboard/courseConfirmation.js
@@ -2,65 +2,62 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Layer, Box, Header, Heading, Footer, Button } from 'grommet';
 
-class CourseConfirmationLayer extends React.Component {
-  render() {
-    return (
-      <Layer
-        closer
+const CourseConfirmationLayer = props => (
+  <Layer
+    closer
+    align="center"
+    onClose={props.setLayer}
+  >
+    <Box
+      classname="LayerBox"
+      direction="column"
+      justify="center"
+      align="center"
+      separator="bottom"
+    >
+      <Header>
+        <Heading
+          align="center"
+          tag="h2"
+        >
+          Confirmation Transaction
+        </Heading>
+      </Header>
+      <Heading
         align="center"
-        onClose={this.props.setLayer}
+        tag="h4"
+      >
+        Are you sure you want to send the transaction for the creation of
+        the Degree Course in {props.courseName} ?
+      </Heading>
+      <Footer
+        justify="between"
+        pad={{ vertical: 'small' }}
       >
         <Box
-          classname="LayerBox"
-          direction="column"
-          justify="center"
+          direction="row"
           align="center"
-          separator="bottom"
+          pad={{ between: 'medium' }}
         >
-          <Header>
-            <Heading
-              align="center"
-              tag="h2"
-            >
-              Conferma invio transazione
-            </Heading>
-          </Header>
-          <Heading
-            align="center"
-            tag="h4"
-          >
-                  Confermi di voler inviare la transazione per la creazione del
-                  Corso di Laurea in {this.courseName} ?
-          </Heading>
-          <Footer
-            justify="between"
-            pad={{ vertical: 'small' }}
-          >
-            <Box
-              direction="row"
-              align="center"
-              pad={{ between: 'medium' }}
-            >
-              <Button
-                label="Confirm"
-                type="submit"
-                primary
-              />
-              <Button
-                label="Cancel"
-                type="reset"
-                primary
-              />
-            </Box>
-          </Footer>
+          <Button
+            label="Confirm"
+            type="submit"
+            primary
+          />
+          <Button
+            label="Cancel"
+            type="reset"
+            primary
+          />
         </Box>
-      </Layer>
-    );
-  }
-}
+      </Footer>
+    </Box>
+  </Layer>
+);
 
 CourseConfirmationLayer.propTypes = {
   setLayer: PropTypes.func.isRequired,
+  courseName: PropTypes.string.isRequired,
 };
 
 export default CourseConfirmationLayer;
