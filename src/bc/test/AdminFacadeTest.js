cb7d2370364b56e140f9fc311a970b18608a0d99
Matteo Todescato <matteotod@gmail.com>
2018-05-03 11:12:00 +0200

    migrating test form .then to async/await

--- src/bc/test/AdminFacadeTest.js
+++ src/bc/test/AdminFacadeTest.js
@@ -1,7 +1,7 @@
 const ListUsers = artifacts.require('./ListUsers.sol');
 const AdminFacade = artifacts.require('./AdminFacade.sol');
 const User = artifacts.require('./User.sol');
-// const AcademicYear = artifacts.require('./AcademicYear.sol');
+const AcademicYear = artifacts.require('./AcademicYear.sol');
 const gAddress = '0xe0d040070bb9e3ebd2cb4ccd37d773387eaec7d4';
 
 
@@ -12,7 +12,7 @@ contract('Testing AdminFacade', () => {
   AdminFacade.deployed().then((inst) => { adminFacadeInstance = inst; });
   ListUsers.deployed().then((inst) => { ListUsersInstance = inst; });
 
-  it('Test insert and get user', () => {
+  it('Test insert and get user',async () => {
     adminFacadeInstance.addUser('simone1', 'ballarin', 'bllsmn7580297584', 12335, gAddress, 0, { from: gAddress });
     ListUsersInstance.getUser.call('0xe0d040070bb9e3ebd2cb4ccd37d773387eaec7d4').then(usr => User.at(usr))
       .then(usr => usr.getSerial())
@@ -23,30 +23,27 @@ contract('Testing AdminFacade', () => {
     ListUsersInstance.getNumberOfUsers().then(result => assert.equal(result.toNumber(), 1, 'added user'));
   });
 
-  it('Test remove user', () => {
+  it('Test remove user', async () => {
     adminFacadeInstance.addUser('mario', 'rossi', 'mrrss7580297584', 12324, '0xe0d040070bb6e3ebd2cb4ccd38d773387eaec7d4', 0);
     ListUsersInstance.getNumberOfUsers().then(result => assert.equal(result.toNumber(), 2, 'added user'));
     adminFacadeInstance.removeUser(gAddress);
     ListUsersInstance.getNumberOfUsers().then(result => assert.equal(result.toNumber(), 1, 'removed user'));
   });
 
-  it('insert an academic year', () => {
+  it('insert an academic year', async () => {
     adminFacadeInstance.addAcademicYear(796, { from: gAddress });
-    adminFacadeInstance.getAcademicYear(796).then((result) => {
-      assert.notEqual(web3.fromAscii(result), 0x0, 'test address academic year');
-    });
+    const AcademicYearAddress = await adminFacadeInstance.getAcademicYear.call(796);
+    assert.notEqual(AcademicYearAddress, '0x0000000000000000000000000000000000000000', 'test address academic year');
   });
-/*
-  it('getAcademicYear retrun correct address', () => {
-    adminFacadeInstance.getAcademicYear(796).then((result) => {
-      console.log();
-      (AcademicYear.at(result)).getNumberOfDC()
-        .then(number => assert.equal(number.toNumber(), 0, 'no degreeCourses'));
-    });
-    adminFacadeInstance.addDegreeCourse(796, 'computer science', 'Mario Rossi', 1);
-    adminFacadeInstance.getAcademicYear(796).then((result) => {
-      (AcademicYear.at(result)).getNumberOfDC()
-        .then(number => assert.equal(number.toNumber(), 1, 'one degree course'));
-    });
-  }); */
+
+  it('getAcademicYear retrun correct address', async () => {
+    adminFacadeInstance.addAcademicYear(797, { from: gAddress });
+    const AcademicYearAddress = await adminFacadeInstance.getAcademicYear(797);
+    const year797 = AcademicYear.at(AcademicYearAddress);
+    let numberOfDC = await year797.getNumberOfDC.call();
+    assert.equal(numberOfDC.toNumber(), 0, 'Before insert');
+    adminFacadeInstance.addDegreeCourse(797, 'computer science', 'Mario Rossi', 1);
+    numberOfDC = await year797.getNumberOfDC.call();
+    assert.equal(numberOfDC.toNumber(), 1, 'After insert');
+  });
 });
