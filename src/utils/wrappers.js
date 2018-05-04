c0d5dcd659026a03607467dd09bf2e76d73ab98a
Denis <denismazzu@live.it>
2018-05-04 15:48:12 +0200

    fix for MenuEntries reducer

--- src/utils/wrappers.js
+++ src/utils/wrappers.js
@@ -4,21 +4,21 @@ import { routerActions } from 'react-router-redux';
 
 export const userIsAuthenticatedStudent = connectedRouterRedirect({
   redirectPath: '/',
-  authenticatedSelector: state => state.userReducer.data !== null && state.userReducer.type === 'student',
+  authenticatedSelector: state => state['user-reducer'].data !== null && state['user-reducer'].type === 'student',
   redirectAction: routerActions.replace,
   wrapperDisplayName: 'UserIsAuthenticatedStudent',
 });
 
 export const userIsAuthenticatedAdmin = connectedRouterRedirect({
   redirectPath: '/',
-  authenticatedSelector: state => state.userReducer.data !== null && state.userReducer.type === 'admin',
+  authenticatedSelector: state => state['user-reducer'].data !== null && state['user-reducer'].type === 'admin',
   redirectAction: routerActions.replace,
   wrapperDisplayName: 'UserIsAuthenticatedAdmin',
 });
 
 export const userIsAuthenticatedProfessor = connectedRouterRedirect({
   redirectPath: '/',
-  authenticatedSelector: state => state.userReducer.data !== null && state.userReducer.type === 'professor',
+  authenticatedSelector: state => state['user-reducer'].data !== null && state['user-reducer'].type === 'professor',
   redirectAction: routerActions.replace,
   wrapperDisplayName: 'UserIsAuthenticatedProfessor',
 });
