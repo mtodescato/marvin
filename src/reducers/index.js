6076ea35094bff312b43f617fcaa4c20b3c80fce
Denis <denismazzu@live.it>
2018-05-05 10:58:02 +0200

    change old UserReducer to new Web3UserInfo reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -16,16 +16,16 @@ import { combineReducers } from 'redux';
 import BookletInfo from './bookletInfo';
 import AddUser from './addUser';
 import Web3 from './web3';
+import Web3UserInfo from './web3UserInfo';
 import MenuEntries from './menuEntries';
-import UserReducer from './userReducer';
 import ListUsers from './listUsers';
 
 export default combineReducers({
   [BookletInfo.store]: BookletInfo.reducer,
   [AddUser.store]: AddUser.reducer,
   [Web3.store]: Web3.reducer,
+  [Web3UserInfo.store]: Web3UserInfo.reducer,
   [MenuEntries.store]: MenuEntries.reducer,
-  [UserReducer.store]: UserReducer.reducer,
   [ListUsers.store]: ListUsers.reducer,
 });
 
@@ -34,7 +34,7 @@ export {
   BookletInfo,
   AddUser,
   Web3,
+  Web3UserInfo,
   MenuEntries,
-  UserReducer,
   ListUsers,
 };
---
+++ src/reducers/web3UserInfo.js
@@ -0,0 +1,37 @@
+import AsyncFlow from './asyncFlow';
+
+export default AsyncFlow({
+  store: 'web-3-user-info',
+  initialState: {
+    data: {
+      name: '',
+      surname: '',
+      solcialNumber: '',
+    },
+    type: 'undefined',
+  },
+  actions: ['WEB_3_USER_INFO'],
+}).extend({
+  reducer: (state, action, { types }) => {
+    switch (action.type) {
+      case types.WEB_3_ADDRESS_SUCCESS:
+        return { ...state, data: action.payload.data, type: action.payload.type };
+      default:
+        return state;
+    }
+  },
+  creators: ({ types }) => ({
+    web3UserInfoRequest: () => ({
+      type: types.WEB_3_USER_INFO_REQUEST,
+    }),
+    web3UserInfoSuccess: ({ data, type }) => ({
+      type: types.WEB_3_USER_INFO_SUCCESS,
+      payload: { data, type },
+    }),
+    web3UserInfoFailed: error => ({
+      type: types.WEB_3_USER_INFO_FAILED,
+      error: true,
+      payload: { error },
+    }),
+  }),
+});
