d5bcf20acf49f1a9ac32af76e0bc7ad426618887
Denis <denismazzu@live.it>
2018-05-05 10:44:11 +0200

    removed merge conflict

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -15,35 +15,26 @@ import { combineReducers } from 'redux';
 // import all reducers
 import BookletInfo from './bookletInfo';
 import AddUser from './addUser';
-<<<<<<< HEAD
 import Web3 from './web3';
-=======
 import MenuEntries from './menuEntries';
 import UserReducer from './userReducer';
 import ListUsers from './listUsers';
->>>>>>> origin/master
 
 export default combineReducers({
   [BookletInfo.store]: BookletInfo.reducer,
   [AddUser.store]: AddUser.reducer,
-<<<<<<< HEAD
   [Web3.store]: Web3.reducer,
-=======
   [MenuEntries.store]: MenuEntries.reducer,
   [UserReducer.store]: UserReducer.reducer,
   [ListUsers.store]: ListUsers.reducer,
->>>>>>> origin/master
 });
 
 // export all reducers
 export {
   BookletInfo,
   AddUser,
-<<<<<<< HEAD
   Web3,
-=======
   MenuEntries,
   UserReducer,
   ListUsers,
->>>>>>> origin/master
 };
