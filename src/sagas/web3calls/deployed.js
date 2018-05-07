b41197d4757525d8df67b59f48eef24acd69a10a
Denis <denismazzu@live.it>
2018-05-07 16:55:35 +0200

    resolve TODO comment

--- src/sagas/web3calls/deployed.js
+++ src/sagas/web3calls/deployed.js
@@ -14,12 +14,21 @@ import contract from 'truffle-contract';
 export const undefinedWeb3Account = 'web3 is defined but accounts has wrong path under web3';
 
 // return metamask accounts
-export const getAccount = () => window.web3.eth.accounts[0]; // TODO: add try and catch
+export const getAccount = () => {
+  const address = window.web3.eth.accounts[0];
+  if (address) return address;
+  throw new Error(undefinedWeb3Account);
+};
+
+const openContract = ({ contractModule, address, method }) => {
+  if (method !== 'at' && method !== 'deployed') throw new Error('undefined method');
+  const wrapper = contract(contractModule);
+  wrapper.setProvider(window.web3.currentProvider);
+  return method === 'at' ? wrapper[method](address) : wrapper[method]();
+};
 
 // open the contractModule and return a promise with contractModule's instance
 // TODO: no tests has been provided for this function
-export const deployed = (contractModule) => {
-  const wrapper = contract(contractModule);
-  wrapper.setProvider(window.web3.currentProvider);
-  return wrapper.deployed();
-};
+export const deployed = contractModule => openContract({ contractModule, method: 'deployed' });
+
+export const at = (contractModule, address) => openContract({ contractModule, address, method: 'at' });
