846575a071a3f31361211e37d19fedc1d413a910
Denis <denismazzu@live.it>
2018-05-07 16:57:19 +0200

    update Web3UserInfoSaga with web3 getter

--- src/sagas/web3UserInfo.js
+++ src/sagas/web3UserInfo.js
@@ -1,65 +1,12 @@
 import { put, takeEvery, call } from 'redux-saga/effects';
 import { Web3UserInfo } from '../reducers';
-import { deployed } from './web3calls';
-import ListUsers from '../bc/build/contracts/ListUsers.json';
-import User from '../bc/build/contracts/User.json';
-
-// FIXIT: risolve la transazione prima ancora di aver ricevuto i dati
-// probabilmente da ricavare l'istanza di User come faccion con ListUsers
-export const getUserInfo = address => new Promise((resolve) => {
-  deployed(ListUsers)
-    .then(async (ListUsersInstance) => {
-      const userType = await ListUsersInstance.getType.call(address);
-      const userAddress = await ListUsersInstance.getUser.call(address);
-      const user = window.web3.eth.contract(User.abi).at(userAddress);
-      let name;
-      user.getName((err, res) => {
-        if (!err) {
-          name = window.web3.toAscii(res);
-        } else {
-          throw new Error(err);
-        }
-      });
-      let surname;
-      await user.getName((err, res) => {
-        if (!err) {
-          surname = window.web3.toAscii(res);
-        } else {
-          throw new Error(err);
-        }
-      });
-      let socialNumber;
-      await user.getName((err, res) => {
-        if (!err) {
-          socialNumber = window.web3.toAscii(res);
-        } else {
-          throw new Error(err);
-        }
-      });
-      let serial;
-      await user.getName((err, res) => {
-        if (!err) {
-          serial = res;
-        } else {
-          throw new Error(err);
-        }
-      });
-      resolve({
-        data: {
-          name,
-          surname,
-          socialNumber,
-          serial,
-        },
-        userType: userType.c[0],
-      });
-    });
-});
+import { getUserInfo, getUserType } from './web3calls/getter';
 
 export function* runAction({ payload }) {
   try {
-    const user = yield call(getUserInfo, payload.address);
-    yield put(Web3UserInfo.creators.web3UserInfoSuccess(user));
+    const userType = yield call(getUserType, payload.address);
+    const data = yield call(getUserInfo, payload.address);
+    yield put(Web3UserInfo.creators.web3UserInfoSuccess({ userType, data }));
   } catch (e) {
     yield put(Web3UserInfo.creators.web3UserInfoFailed(e.message)); // fail the promise getWeb3
   }
