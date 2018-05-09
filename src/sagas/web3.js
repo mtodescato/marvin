0d8b40d00189c52ad65c769b7e56ac61cbd92f49
Denis <denismazzu@live.it>
2018-05-09 20:25:44 +0200

    fixed web3 saga

--- src/sagas/web3.js
+++ src/sagas/web3.js
@@ -1,32 +1,9 @@
-import { put, takeEvery, call } from 'redux-saga/effects';
-import Web3 from 'web3';
+import { put, takeEvery } from 'redux-saga/effects';
 import { Web3 as Web3Reducer, Web3UserInfo } from '../reducers';
 import { getAccount } from './web3calls/deployed';
 
-export const undefinedMetamask = 'Mist/MetaMask\'s provider isn\'t be recognized, please use it before run marvin';
-
-export const getWeb3 = () => new Promise((resolve, reject) => {
-  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-  let result;
-  if (typeof window.web3 !== 'undefined') {
-    // Use Mist/MetaMask's provider.
-    result = new Web3(window.web3.currentProvider);
-    global.window.web3 = result; // global var setted
-    resolve(result);
-  } else {
-    reject(Error(undefinedMetamask));
-  }
-});
-
 export function* runAction() {
-  try {
-    yield call(getWeb3);
-    const address = getAccount();
-    yield put(Web3Reducer.creators.web3AddressSuccess(address));
-    yield put(Web3UserInfo.creators.web3UserInfoRequest(address));
-  } catch (e) {
-    yield put(Web3Reducer.creators.web3AddressFailed(e.message)); // fail the promise getWeb3
-  }
+  yield put(Web3UserInfo.creators.web3UserInfoRequest(getAccount()));
 }
 
 export function* triggerAction() {
