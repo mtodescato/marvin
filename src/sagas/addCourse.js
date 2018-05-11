50c4d494760f0adb052aaac4510c809afe0abdd9
Denis <denismazzu@live.it>
2018-05-11 20:47:03 +0200

    add AddCourse saga

---
+++ src/sagas/addCourse.js
@@ -0,0 +1,17 @@
+import { put, takeLatest, call } from 'redux-saga/effects';
+import { AddCourse } from '../reducers';
+import { addCourse } from './web3calls/getter';
+
+export function* runAction({ payload }) {
+  try {
+    yield call(addCourse, payload.course);
+    yield put(AddCourse.creators.addCourseSuccess());
+  } catch (e) {
+    yield put(AddCourse.creators.addCourseFailed(e.message));
+  }
+}
+
+export function* triggerAction() {
+  yield takeLatest(AddCourse.types.ADD_COURSE_REQUEST, runAction);
+}
+
