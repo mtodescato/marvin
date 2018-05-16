f65ce6979d1167d0e912272b6e496911907a6288
JZambo9 <JZambo93@gmail.com>
2018-05-16 16:26:24 +0200

    added props

--- src/containers/admin/ListCourses.js
+++ src/containers/admin/ListCourses.js
@@ -1,5 +1,8 @@
 import React from 'react';
+// import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import ListCoursesComponent from '../../components/admin/ListCoursesComponent';
+// import { ListCourses as ListCoursesReducer } from '../../reducers';
 
 export const courses = [{
   ID: '43553',
@@ -21,13 +24,49 @@ export const courses = [{
 },
 ];
 
-const address = {};
+class ListCourses extends React.Component {
+  /*
+  componentWillMount() {
+    this.props.initialize();
+  } */
 
-const ListCourses = () => (
-  <ListCoursesComponent
-    courses={courses}
-    address={address}
-  />
-);
+  justfortheLint() { // da eliminare
+    this.size = 0;
+  }
+
+  render() {
+    return (
+      <ListCoursesComponent
+        size={this.props.size}
+        coursesEntries={courses}
+        // coursesEntries={this.props.courses}
+      />
+    );
+  }
+}
+
+ListCourses.propTypes = {
+  /* courses: PropTypes.arrayOf(PropTypes.shape({
+    ID: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    president: PropTypes.number.isRequired,
+    courseType: PropTypes.string.isRequired,
+  })).isRequired,
+  */
+  size: PropTypes.number.isRequired,
+  // initialize: PropTypes.func.isRequired,
+};
+/*
+const mapDispatchToProps = dispatch => ({
+  initialize: () => { dispatch(ListCoursesReducer.creators.listCoursesRequest()); },
+});
+
+const mapStateToProps = state => ({
+  courses: state['list-courses'].courses,
+  size: state['list-courses'].size,
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ListCourses);
+*/
 
 export default ListCourses;
