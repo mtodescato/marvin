5f6557d540d3a8434196fbb9962d83dd41146eb0
JZambo9 <JZambo93@gmail.com>
2018-05-17 15:54:07 +0200

    created professor view

---
+++ src/views/professor.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Section, Split, Box } from 'grommet';
+import MenuProfessor from '../containers/professor/MenuProfessor';
+import MainPanel from '../containers/student/MainPanel';
+import MarvinHeader from '../containers/shared/MarvinHeader';
+import MarvinFooter from '../containers/shared/MarvinFooter';
+
+const Professor = () => (
+  <div className="Professor">
+    <MarvinHeader title="Professor Dashboard" />
+    <Section pad="none">
+      <Split
+        flex="right"
+        fixed
+      >
+        <Box
+          colorIndex="light-2"
+          justify="center"
+          align="center"
+          separator="vertical"
+        >
+          <MenuProfessor />
+        </Box>
+        <Box >
+          <MainPanel />
+        </Box>
+      </Split>
+    </Section>
+    <MarvinFooter />
+  </div>
+);
+export default Professor;
