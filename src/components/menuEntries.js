76bae1ce6a051f1414563dba733cc6aee2e33069
JZambo9 <JZambo93@gmail.com>
2018-05-17 15:54:42 +0200

    created entries for both Professor and Student menu

---
+++ src/components/menuEntries.js
@@ -0,0 +1,15 @@
+export const menuEntriesStudent = [
+  'Booklet',
+  'Courses',
+  'Exams',
+  'Results',
+  'Degree',
+];
+
+export const menuEntriesProfessor = [
+  'Prof',
+  'Courses',
+  'Exams',
+  'Results',
+  'Degree',
+];
