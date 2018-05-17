06010495e7171529849cd72a496feae3dd687a16
JZambo9 <JZambo93@gmail.com>
2018-05-17 11:20:17 +0200

    fix content position and added list items

--- src/components/admin/ConfirmationDelete.js
+++ src/components/admin/ConfirmationDelete.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Box, Layer, Form, Header, Heading, FormFields, Paragraph, Footer, Button } from 'grommet';
+import { Box, Layer, Header, Heading, Paragraph, Footer, Button, List, ListItem, Label } from 'grommet';
 
 class ConfirmationDelete extends React.Component {
   constructor(props) {
@@ -18,40 +18,100 @@ class ConfirmationDelete extends React.Component {
       <Layer
         closer
         onClose={this.props.setLayer}
+        align="center"
+        flush
       >
-        <Box pad="medium">
-          <Form>
-            <Header>
-              <Heading>
+        <Box
+          pad={{ vertical: 'small', horizontal: 'small' }}
+        >
+          <Header
+            colorIndex="light-2"
+            justify="center"
+            full="horizontal"
+            pad={{ vertical: 'none', horizontal: 'none', between: 'medium' }}
+          >
+            <Heading tag="h3" strong>
                 Delete user confirmation
-              </Heading>
-            </Header>
-            <FormFields>
-              <fieldset>
-                <Paragraph>
-                Are you sure you want to delete the user: <br />
-                  {this.props.userName} <br />
-                  {this.props.userSurname} <br />
-                  {this.props.userAddress} <br />
-                  {this.props.userRole} <br />
-                </Paragraph>
-              </fieldset>
-            </FormFields>
-            <Footer pad={{ vertical: 'medium' }}>
-              <Button
-                label="Conferma"
-                type="submit"
-                primary
-                onClick={this.onDelete}
-              />
-              <Button
-                label="Annulla"
-                type="submit"
-                primary
-                onClick={this.props.setLayer}
-              />
-            </Footer>
-          </Form>
+            </Heading>
+          </Header>
+          <Paragraph margin="small" align="center">
+                Are you sure you want to delete the user?
+          </Paragraph>
+          <Box pad={{ vertical: 'small', horizontal: 'none' }}>
+            <List >
+              <ListItem
+                justify="between"
+                separator="bottom"
+                pad={{ vertical: 'none', horizontal: 'small', between: 'medium' }}
+                margin="none"
+              >
+                <Heading tag="h4" margin="none">
+                Name:
+                </Heading>
+                <Heading tag="h4" margin="none">
+                  {this.props.userName}
+                </Heading>
+              </ListItem>
+              <ListItem
+                justify="between"
+                separator="bottom"
+                pad={{ vertical: 'none', horizontal: 'small', between: 'medium' }}
+                margin="none"
+              >
+                <Heading tag="h4" margin="none">
+                Surname:
+                </Heading>
+                <Heading tag="h4" margin="none">
+                  {this.props.userSurname}
+                </Heading>
+              </ListItem>
+              <ListItem
+                justify="between"
+                separator="bottom"
+                pad={{ vertical: 'none', horizontal: 'small', between: 'medium' }}
+                margin="none"
+              >
+                <Heading tag="h4" margin="none">
+                Address:
+                </Heading>
+                <Label size="small">
+                  {this.props.userAddress}
+                </Label>
+              </ListItem>
+              <ListItem
+                justify="between"
+                separator="bottom"
+                pad={{ vertical: 'none', horizontal: 'small', between: 'medium' }}
+                margin="none"
+              >
+                <Heading tag="h4" margin="none">
+                Role:
+                </Heading>
+                <Heading tag="h4" margin="none">
+                  {this.props.userRole}
+                </Heading>
+              </ListItem>
+            </List>
+          </Box>
+          <Footer
+            justify="center"
+            align="center"
+            pad={{ horizontal: 'none', vertical: 'small', between: 'medium' }}
+            direction="row"
+          >
+            <Button
+              label="Confirm"
+              type="submit"
+              primary
+              onClick={this.onDelete}
+            />
+            <Button
+              label="Cancel"
+              type="submit"
+              primary
+              onClick={this.props.setLayer}
+            />
+          </Footer>
         </Box>
       </Layer>
     );
