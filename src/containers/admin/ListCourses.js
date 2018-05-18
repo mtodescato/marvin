0e7a7e5a523ac7e4590f17ed936d5e4e6d7d37fe
sballari <ballarinsimone@gmail.com>
2018-05-18 11:41:23 +0200

    validazione delle props

--- src/containers/admin/ListCourses.js
+++ src/containers/admin/ListCourses.js
@@ -1,72 +1,42 @@
 import React from 'react';
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import ListCoursesComponent from '../../components/admin/ListCoursesComponent';
-// import { ListCourses as ListCoursesReducer } from '../../reducers';
-
-export const courses = [{
-  ID: '43553',
-  name: 'Analisi',
-  president: 'Francescopaolo Montefalcone',
-  courseType: 'Three-year',
-},
-{
-  ID: '43553',
-  name: 'Analisi',
-  president: 'Francescopaolo Montefalcone',
-  courseType: 'Master',
-},
-{
-  ID: '43553',
-  name: 'Analisi',
-  president: 'Francescopaolo Montefalcone',
-  courseType: 'Three-year',
-},
-];
+import { ListStudyCourses } from '../../reducers';
 
 class ListCourses extends React.Component {
-  /*
   componentWillMount() {
-    this.props.initialize();
-  } */
-
-  justfortheLint() { // da eliminare
-    this.size = 0;
+    this.props.initialize(2018);
   }
 
   render() {
     return (
       <ListCoursesComponent
         size={this.props.size}
-        coursesEntries={courses}
-        // coursesEntries={this.props.courses}
+        coursesEntries={this.props.courses}
       />
     );
   }
 }
 
 ListCourses.propTypes = {
-  /* courses: PropTypes.arrayOf(PropTypes.shape({
+  courses: PropTypes.arrayOf(PropTypes.shape({
     ID: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
-    president: PropTypes.number.isRequired,
+    president: PropTypes.string.isRequired,
     courseType: PropTypes.string.isRequired,
   })).isRequired,
-  */
   size: PropTypes.number.isRequired,
-  // initialize: PropTypes.func.isRequired,
+  initialize: PropTypes.func.isRequired,
 };
-/*
+
 const mapDispatchToProps = dispatch => ({
-  initialize: () => { dispatch(ListCoursesReducer.creators.listCoursesRequest()); },
+  initialize: (year) => { dispatch(ListStudyCourses.creators.listStudyCoursesRequest(year)); },
 });
 
 const mapStateToProps = state => ({
-  courses: state['list-courses'].courses,
-  size: state['list-courses'].size,
+  courses: state['list-study-courses'].courses,
+  size: state['list-study-courses'].size,
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ListCourses);
-*/
-
-export default ListCourses;
