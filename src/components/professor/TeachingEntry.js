59350b316d0d453f274ed7c967fb7072c62a7065
JZambo9 <JZambo93@gmail.com>
2018-05-19 01:13:05 +0200

    filled ListTeachings components

---
+++ src/components/professor/TeachingEntry.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Accordion, AccordionPanel } from 'grommet';
+import ExamsTeaching from '../../containers/professor/ExamsTeaching';
+
+const TeachingEntry = props => (
+  <Accordion>
+    <AccordionPanel
+      pad="none"
+      heading={`${props.index + 1}.  ${props.name} - ${props.course}`}
+    >
+      <ExamsTeaching />
+    </AccordionPanel>
+  </Accordion>
+);
+
+
+TeachingEntry.propTypes = {
+  index: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  course: PropTypes.string.isRequired,
+};
+
+export default TeachingEntry;
