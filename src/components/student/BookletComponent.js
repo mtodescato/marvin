63507466c80dc524252b6c7dff05a2f3ad006063
Denis <denismazzu@live.it>
2018-05-22 13:47:30 +0200

    fix react comopnents

--- src/components/student/BookletComponent.js
+++ src/components/student/BookletComponent.js
@@ -8,51 +8,43 @@ import notFound from '../../images/404.gif';
 
 class BookletComponent extends React.Component {
   componentWillMount() {
-    this.props.bookletInfoRequest(this.props.address);
+    this.props.bookletInfoRequest();
   }
   render() {
     switch (this.props.status) {
       case 'RESOLVED':
-        if (this.props.booklet.user !== undefined && this.props.booklet.exams !== undefined) {
-          return (
-            <Animate
-              enter={{ animation: 'fade', duration: 1000, delay: 0 }}
-              keep
-            >
-              <div>
-                <h2> Welcome {this.props.booklet.user.name} {this.props.booklet.user.surname}
-                  {' '} n. {this.props.booklet.user.matricola}
-                </h2>
-                <h4> Weighted Average: {this.props.booklet.user.media}</h4>
-                <h3> Exams in your Booklet</h3>
-                <Table responsive>
-                  <thead>
-                    <tr>
-                      <th>Referent</th>
-                      <th>Name</th>
-                      <th>CFU</th>
-                      <th>State</th>
-                      <th>Result</th>
-                      <th>Date</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {
-            this.props.booklet.exams.map(element => (
-              <ExamEntry {...element} />
-            ))
-          }
-                  </tbody>
-                </Table>
-              </div>
-            </Animate>
-          );
-        }
         return (
-          <Section align="center">
-            <Image src={loading} size="medium" />
-            <h3>Receiving data ...</h3>
-          </Section>
+          <Animate
+            enter={{ animation: 'fade', duration: 1000, delay: 0 }}
+            keep
+          >
+            <div>
+              <h2> Welcome {this.props.user.name} {this.props.user.surname}
+                {' '} n. {this.props.user.matricola}
+              </h2>
+              <h4> Weighted Average: {this.props.user.media}</h4>
+              <h3> Exams in your Booklet</h3>
+              <Table responsive>
+                <thead>
+                  <tr>
+                    <th>Referent</th>
+                    <th>Name</th>
+                    <th>CFU</th>
+                    <th>State</th>
+                    <th>Result</th>
+                    <th>Date</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {
+          this.props.exams.map(element => (
+            <ExamEntry {...element} />
+          ))
+        }
+                </tbody>
+              </Table>
+            </div>
+          </Animate>
         );
       case 'PENDING':
         return (
@@ -69,7 +61,7 @@ class BookletComponent extends React.Component {
             align="center"
           >
             <Image src={notFound} size="large" />
-            <Button onClick={this.props.bookletInfoRequest(this.props.address)} label="Try again" />
+            <Button onClick={this.props.bookletInfoRequest()} label="Try again" />
           </Section>
         );
       default:
@@ -78,7 +70,7 @@ class BookletComponent extends React.Component {
             align="center"
           >
             <Image src={notFound} size="large" />
-            <Button onClick={this.props.bookletInfoRequest(this.props.address)} label="Try again" />
+            <Button onClick={this.props.bookletInfoRequest()} label="Try again" />
           </Section>
         );
     }
@@ -86,9 +78,9 @@ class BookletComponent extends React.Component {
 }
 
 BookletComponent.propTypes = {
-  booklet: PropTypes.arrayOf.isRequired,
+  user: PropTypes.arrayOf.isRequired,
+  exams: PropTypes.arrayOf.isRequired,
   bookletInfoRequest: PropTypes.func.isRequired,
-  address: PropTypes.string.isRequired,
   status: PropTypes.string.isRequired,
 };
 
