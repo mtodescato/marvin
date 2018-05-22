e55502419506f9822fcef750d2e03d1d2ccf94e6
Matteo Todescato <matteotod@gmail.com>
2018-05-22 14:37:10 +0200

    added docs to DegreeRequests.sol

--- src/bc/contracts/DegreeRequests.sol
+++ src/bc/contracts/DegreeRequests.sol
@@ -17,25 +17,35 @@ contract DegreeRequests {
         address professorContract;
     }
 
+    /** @dev Check if the the sender is the admin facade.*/
     modifier onlyAdmin() {
         require(msg.sender == adminFacade);
         _;
     }
 
+    /** @dev Check if the the sender is the student facade.*/
     modifier onlyStudent() {
         require(msg.sender == studentFacade);
         _;
     }
 
+    /** @dev Check if the student doesn't have a pending degree request.
+    *   @param studentContract address of the student contract. 
+    */
     modifier onlyNewRequest(address studentContract) {
         require(studentToInt[studentContract] == 0);
         _;
     }
 
+    /** dev Constructor of admin facade initialize the adminFacade field with msg.sender. */
     constructor() public {
         adminFacade = msg.sender;
     }
 
+    /** @dev Change the two accounts with write permissions on the contract storage.
+    *   @param newAdmin address of the new admin.
+    *   @param newStudent address of the new student. 
+    */
     function changeOwners(address newAdmin, address newStudent) public onlyAdmin() {
         if (newAdmin != address(0) && newStudent != address(0)) {
             adminFacade = newAdmin;
@@ -43,6 +53,12 @@ contract DegreeRequests {
         }
     }
 
+    /** @dev Add a degree request to the mapping.
+    *   @param studentContract address of the student contract creating the degree request.
+    *   @param thesisTitle title of the student thesis.
+    *   @param submmissionDate date of the subbmission of the degree request.
+    *   @param professorContract address of the contract of the professor. 
+    */
     function addRequest(address studentContract, bytes thesisTitle, bytes submmissionDate, address professorContract)
     public
     onlyStudent()
@@ -54,10 +70,17 @@ contract DegreeRequests {
         last += 1;
     }
 
+    /** @dev return the number of pending requests.
+    *   @return uint number of pending requests.
+    */
     function pendingDegreeRequestNumber() public view returns(uint) {
         return last;
     }
 
+    /** @dev Change the state of the degree request.
+    *   @param newState int8 rapresenting the new state of the request.
+    *   @param request index of the degree request. 
+    */
     function manageRequest(int8 newState, uint request) public onlyAdmin() {
         require(newState == -1 || newState == 1);
         require(request < last);
@@ -68,6 +91,13 @@ contract DegreeRequests {
         last -= 1;
     }
 
+    /** @dev return the degree request given an index.
+    *   @param index uint of the degree request.
+    *   @return bytes title of the thesis.
+    *   @return int8 state of the degree request.
+    *   @return bytes date of submission of the request.
+    *   @return address address of the professor contract.
+    */
     function getDegreeRequestIndex(uint index) public view returns( bytes, int8, bytes, address) {
         require(index < last);
         address stdCont = intToSudent[index];
@@ -75,6 +105,13 @@ contract DegreeRequests {
         return ( request.thesisTitle, request.requestState, request.submmissionDate, request.professorContract);
     }
 
+    /** @dev return the degree request given a student address.
+    *   @param studentContract address of the student of the degree request.
+    *   @return bytes title of the thesis.
+    *   @return int8 state of the degree request.
+    *   @return bytes date of submission of the request.
+    *   @return address address of the professor contract.
+    */
     function getDegreeRequest(address studentContract) public view returns( bytes, int8, bytes, address) {
         require(studentContractToRequest[studentContract].professorContract != 0x0);
         DegreeRequest memory request = studentContractToRequest[studentContract];
