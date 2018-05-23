2891b29cebc0e4609967d7cb909330857dd33cf0
Denis <denismazzu@live.it>
2018-05-23 17:39:56 +0200

    fix saga modules

--- src/sagas/Student/bookletInfo.js
+++ src/sagas/Student/bookletInfo.js
@@ -1,11 +1,17 @@
 import { put, takeLatest, call } from 'redux-saga/effects';
 import { BookletInfo } from '../../reducers';
-import { getStudentInfo, getStudentTeachings } from '../web3calls/getter';
+import { getStudentInfo, getStudentTeachings, getPassedTeachings, addExamToPassedTeaching } from '../web3calls/getter';
 
 export function* runAction() {
   try {
     const user = yield call(getStudentInfo);
     const exams = yield call(getStudentTeachings);
+    const passedAddresses = yield call(getPassedTeachings);
+    for (let i = 0; i < exams.length; i += 1) {
+      if (passedAddresses.filter(add => add === exams[i].address).length) {
+        exams[i] = yield call(addExamToPassedTeaching, exams[i]);
+      }
+    }
     const booklet = { user, exams };
     yield put(BookletInfo.creators.bookletInfoSuccess(booklet));
   } catch (e) {
--- src/sagas/web3calls/getter/student.js
+++ src/sagas/web3calls/getter/student.js
@@ -1,13 +1,12 @@
 import { deployed, getAccount, at } from '../deployed';
 import { getUserInfoFromCAddress, getNumberOfTeachings, getTeachings, getCourseNameFromAddress } from '.';
-// import { createArray } from '../../../utils/global';
+import { createArray } from '../../../utils/global';
 import { getUserContractAddress } from './contract';
 
 import StudentFacade from '../../../bc/build/contracts/StudentFacade.json';
 // import DegreeCourse from '../../../bc/build/contracts/DegreeCourse.json';
 import Student from '../../../bc/build/contracts/Student.json';
-// import Exam from '../../../bc/build/contracts/Exam.json';
-// import Teaching from '../../../bc/build/contracts/Teaching.json';
+import Exam from '../../../bc/build/contracts/Exam.json';
 
 const studentContractAddress = () => getUserContractAddress(getAccount());
 
@@ -21,40 +20,49 @@ export const setActiveDegreeCourse = degreeAddress => deployed(StudentFacade)
     return inst.setDegreeCourse(degreeAddress, stdC, { from: getAccount() });
   });
 
+const getPassedTeachingAddress = index => deployed(StudentFacade)
+  .then(async (inst) => {
+    const stdC = await studentContractAddress();
+    return inst.getTeaching.call(index, stdC);
+  });
 
-// export const getStudentNumberOfTeachings = () => deployed(StudentFacade)
-//   .then(async (inst) => {
-//     const studentAddress = await studentContractAddress();
-//     return inst.getNumberOfTeachings.call(studentAddress);
-//   }).then(Number);
 
-// const getStudentTeachingInfo = index => deployed(StudentFacade)
-//   .then(inst => inst.getTeaching.call(index, studentContractAddress()))
-//   .then(teachingAddress => at(Teaching, teachingAddress))
-//   .then(async teaching => ({
-//     address: teaching.address,
-//     nome: await teaching.getName.call(),
-//     responsabile: await teaching.getReferenceProfessor.call(),
-//     exam: await teaching.getExam.call(0),
-//     stato: 'subscribed',
-//   }));
+const getNumberOfPassedTeachings = () => deployed(StudentFacade)
+  .then(async (inst) => {
+    const stdC = await studentContractAddress();
+    return inst.getNumberOfTeachings.call(stdC);
+  }).then(Number);
 
-// export const getStudentTeachings = async size =>
-//   Promise.all(createArray(size)
-//     .map(index => getStudentTeachingInfo(index)));
+export const getPassedTeachings = async () =>
+  Promise.all(createArray(await getNumberOfPassedTeachings())
+    .map(index => getPassedTeachingAddress(index)));
 
-const addExamProperties = teaching => teaching;
+export const addExamToPassedTeaching = async (teaching) => {
+  const stdC = await studentContractAddress();
+  return deployed(StudentFacade)
+    .then(async inst => inst.getExam.call(teaching.address, stdC))
+    .then(examAdd => at(Exam, examAdd)
+      .then(async exam => ({
+        ...teaching,
+        data: window.web3.toAscii(await exam.getDate.call()),
+        voto: Number(await exam.getMark.call(stdC)),
+      })));
+};
 
 export const getStudentTeachings = async () => {
-  const course = await getActiveDegreeCourse(await studentContractAddress());
+  const stdC = await studentContractAddress();
+  const course = await getActiveDegreeCourse(stdC);
   if (course.name === 'N/A') return [];
   const size = await getNumberOfTeachings(course.address);
   const teachings = await getTeachings({ course: course.address, size });
-  return teachings.map(addExamProperties);
+  return teachings.map(teaching => ({
+    ...teaching,
+    nome: teaching.name,
+    responsabile: teaching.responsible,
+    stato: 'Pending',
+    data: 'To be Define',
+  }));
 };
-
-export const getSubscribedExams = () => [];
-export const getPassedExams = () => [];
 
 const getAverage = () => 18;
 
