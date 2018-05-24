47075d512da3d6c50054c9f846d81bd17aa6a3f7
Denis <denismazzu@live.it>
2018-05-24 11:14:34 +0200

    getPassedTeachings is a subtypes of getSubscribedExams

--- src/sagas/Student/listBookingExams.js
+++ src/sagas/Student/listBookingExams.js
@@ -1,15 +1,14 @@
 import { put, takeLatest, call } from 'redux-saga/effects';
 import { ListBookingExams } from '../../reducers';
-import { getStudentTeachings, getPassedTeachings, getSubscribedExams } from '../web3calls/getter';
+import { getStudentTeachings, getSubscribedExams } from '../web3calls/getter';
 
 export function* runAction() {
   try {
     let exams = yield call(getStudentTeachings);
-    const passedAddresses = yield call(getPassedTeachings);
     const subscribedExams = yield call(getSubscribedExams);
+    console.log(subscribedExams);
     for (let i = 0; i < exams.length; i += 1) {
-      exams = exams.filter(exam => passedAddresses.indexOf(exam.address) === -1 &&
-        subscribedExams.indexOf(exam.address) === -1);
+      exams = exams.filter(exam => subscribedExams.indexOf(exam.address) === -1);
     }
     yield put(ListBookingExams.creators.listBookingExamsSuccess(exams));
   } catch (e) {
