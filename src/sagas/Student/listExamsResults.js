05917f40c29d10a9505f92885fc0461c2b4a9791
Denis <denismazzu@live.it>
2018-05-24 16:40:09 +0200

    fix saga for ListExamsResults

--- src/sagas/Student/listExamsResults.js
+++ src/sagas/Student/listExamsResults.js
@@ -1,10 +1,14 @@
-import { put, takeLatest /* , call */ } from 'redux-saga/effects';
+import { put, takeLatest, call } from 'redux-saga/effects';
 import { ListExamsResults } from '../../reducers';
-// import { getStudentTeachings, getSubscribedExams } from '../web3calls/getter';
+import { getStudentTeachings } from '../web3calls/getter';
 
 export function* runAction() {
   try {
-    yield put(ListExamsResults.creators.listExamsResultsSuccess());
+    const teachings = (yield call(getStudentTeachings)).filter(item => item.markStatus === 'subscribed');
+    yield put(ListExamsResults.creators.listExamsResultsSuccess({
+      exams: teachings,
+      size: teachings.length,
+    }));
   } catch (e) {
     yield put(ListExamsResults.creators.listExamsResultsFailed(e.message));
   }
