ac8ee1d9aeef3556694821d8b577a09bf45aa9b0
JZambo9 <JZambo93@gmail.com>
2018-05-24 10:58:43 +0200

    filled ListYears container and components

---
+++ src/components/admin/YearEntry.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import TableRow from 'grommet/components/TableRow';
+
+const YearEntry = props => (
+  <TableRow>
+    <td>{props.index + 1}</td>
+    <td>Academic Year {props.year}/{parseInt(props.year, 10) + 1}</td>
+    <td>{props.address}</td>
+  </TableRow>
+);
+
+
+YearEntry.propTypes = {
+  index: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
+  address: PropTypes.string.isRequired,
+};
+
+export default YearEntry;
