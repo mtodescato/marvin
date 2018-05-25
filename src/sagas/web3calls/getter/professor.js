f80856bf0d236ecaf4c850e5a92a5811f8811030
Denis <denismazzu@live.it>
2018-05-25 10:39:06 +0200

    refactor professor

--- src/sagas/web3calls/getter/professor.js
+++ src/sagas/web3calls/getter/professor.js
@@ -17,12 +17,13 @@ export const addExam = exam => deployed(ProfessorFacade)
 
 const getExamsFromTeachingAdd = teachingAdd => at(Teaching, teachingAdd)
   .then(async (teaching) => {
-    const size = await teaching.getNumberOfExams.call();
+    const size = Number(await teaching.getNumberOfExams.call());
     return Promise.all(createArray(size).map(index => teaching.getExam.call(index)));
-  }).then(examsAdd => Promise.all(examsAdd.map(examAdd => at(Exam, examAdd)
+  }).then(async examsAdd => Promise.all(examsAdd.map(examAdd => at(Exam, examAdd)
     .then(async exam => ({
       ...(await getTeachingInfoFromAdd(teachingAdd)),
       examAddress: exam.address,
+      ID: exam.address,
       date: window.web3.toAscii(await exam.getDate.call()),
       studentsNumber: Number(await exam.getNumberOfStudents.call()),
     })))));
