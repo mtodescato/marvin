591592c436b65391012ee00b86ee902a54f892c0
JZambo9 <JZambo93@gmail.com>
2018-05-30 23:29:31 +0200

    typo

--- src/components/student/ListAvaiableExamsComponent.js
+++ src/components/student/ListAvaiableExamsComponent.js
@@ -11,23 +11,23 @@ const ListAvaiableExamsComponent = ({
   examsEntries,
   subscribeToExam,
 }) => (
-  <Box
-    className="PanelBox"
-    direction="column"
-    margin="small"
-    separator="bottom"
-  >
+  <Box className="PanelBox" direction="column" margin="small" separator="bottom" >
     <Box className="titleBox" align="center" alignSelf="center" colorIndex="brand" full="horizontal" >
       <Heading tag="h2" strong>
-        Booking Exams
+        Exam Session
       </Heading>
     </Box>
 
-    <Box
-      className="searchBox"
-      size="medium"
-      pad={{ horizontal: 'medium', vertical: 'small' }}
-    >
+
+    <Box className="infoBox" pad={{ horizontal: 'medium', vertical: 'small' }} >
+      <Heading tag="h5" >
+            TODO This page shows the academic activities in the student’s booklet. In the case of
+            activities not yet passed and attended, just click ‘Exam Session’ in the menu entry
+            that provides access to the list of the exam sessions.
+      </Heading>
+    </Box>
+
+    <Box className="searchBox" size="medium" pad={{ horizontal: 'medium', vertical: 'small' }} >
       <Heading tag="h4" >
         Exams avaiable: {size}
       </Heading>
