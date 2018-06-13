7f928ad6e027b13545a0bbedd36817dbed2c1f64
Matteo Todescato <matteotod@gmail.com>
2018-06-13 17:11:27 +0200

    update cost page

--- src/components/shared/ShowMeTheCost.js
+++ src/components/shared/ShowMeTheCost.js
@@ -3,18 +3,18 @@ import { Box, Heading, Image, Animate, Table, TableHeader, TableRow } from 'grom
 import ethMetUnipd from '../../images/ethereum_metamask_unipd.png';
 
 export const pricesEntries = [
-  { operation: 'Deploy', gas: 'TODO', price: '25€' },
-  { operation: 'Create User', gas: 'TODO', price: '0,38€ - 0,50€' },
-  { operation: 'Delete User', gas: 'TODO', price: '0,02€ - 0,05€' },
-  { operation: 'Create Academic Year', gas: 'TODO', price: '0,12€ - 0,18€' },
-  { operation: 'Create Degree Course', gas: 'TODO', price: '0,21€ - 0,26€' },
-  { operation: 'Create Teaching', gas: 'TODO', price: '0,21€ - 0,26€' },
-  { operation: 'Degree Course Application', gas: 'TODO', price: '0,02€ - 0,05€' },
-  { operation: 'Create Exam', gas: 'TODO', price: '0,37€ - 0,43€' },
-  { operation: 'Exam Application', gas: 'TODO', price: '0,02€ - 0,05€' },
-  { operation: 'Publish Mark', gas: 'TODO', price: '0,03€ - 0,06€' },
-  { operation: 'Accept Mark', gas: 'TODO', price: '0,04€ - 0,08€' },
-  { operation: 'Reject Mark', gas: 'TODO', price: '0,04€ - 0,08€' },
+  { operation: 'Deploy', price: '25€' },
+  { operation: 'Create User', price: '0,38€ - 0,50€' },
+  { operation: 'Delete User', price: '0,02€ - 0,05€' },
+  { operation: 'Create Academic Year', price: '0,12€ - 0,18€' },
+  { operation: 'Create Degree Course', price: '0,21€ - 0,26€' },
+  { operation: 'Create Teaching', price: '0,21€ - 0,26€' },
+  { operation: 'Degree Course Application', price: '0,02€ - 0,05€' },
+  { operation: 'Create Exam', price: '0,37€ - 0,43€' },
+  { operation: 'Exam Application', price: '0,02€ - 0,05€' },
+  { operation: 'Publish Mark', price: '0,03€ - 0,06€' },
+  { operation: 'Accept Mark', price: '0,04€ - 0,08€' },
+  { operation: 'Reject Mark', price: '0,04€ - 0,08€' },
 ];
 
 const ShowMeTheCost = () => (
@@ -39,13 +39,12 @@ const ShowMeTheCost = () => (
     <Box direction="row" align="center" alignSelf="center" colorIndex="light-2">
       <Animate enter={{ animation: 'fade', duration: 1000, delay: 0 }} keep >
         <Table responsive>
-          <TableHeader labels={['Operation', 'Gas Used', 'Price']} />
+          <TableHeader labels={['Operation', 'Price']} />
           <tbody>
             {
               pricesEntries.map(element => (
                 <TableRow key={element.operation}>
                   <td>{element.operation}</td>
-                  <td>{element.gas}</td>
                   <td>{element.price}</td>
                 </TableRow>
               ))
